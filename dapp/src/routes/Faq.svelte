2d4982547212f2d07988c4bc68c2a331e148a19a
Gregory Rocco <gregoryrocco1226@gmail.com>
2021-05-11 16:45:29 -0400

    Added basic faq to TZP

--- dapp/src/routes/Faq.svelte
+++ dapp/src/routes/Faq.svelte
@@ -7,26 +7,40 @@
   <div class="flex flex-col justify-center flex-grow">
     <TextBody2 value="What is Tezos Profiles?" class="mb-2 font-bold" />
     <TextBody2
-      value="Tezos Profiles enables you to associate your online identity with your Tezos account"
+      value="Tezos Profiles (TZP) is a web application where users can associate their public online identity based on social media profiles and self-attested information to their Tezos address."
       class="mb-4"
     />
     <TextBody2
-      value="Why Would Anyone Want to Do This?"
+      value="How Does it Work?"
       class="mb-2 font-bold"
     />
     <TextBody2
-      value="Tezos Profiles enables you to associate your online identity with your Tezos account"
+      value="Users can build their profiles with the available credentials on Tezos Profiles. Each credential has a unique workflow that must be completed prior to credential issuance. Once issued and uploaded, platforms can then globally resolve this user-managed information to establish a creator’s profile."
       class="mb-4"
     />
     <TextBody2
-      value="What Can I Do With My Tezos Profile?"
+      value="Do You Plan on Supporting Additional Verifications?"
       class="mb-2 font-bold"
     />
     <TextBody2
-      value="Tezos Profiles enables you to associate your online identity with your Tezos account"
+      value="Yes - we plan on continually extending support in TZP to also include credentials from additional public social profiles to give creators the ability to continue to associate their core public profiles."
       class="mb-4"
     />
-    <TextBody2 value="Spruce?" class="mb-2 font-bold" />
-    <TextBody2 value="Yes. :tree:" />
+    <TextBody2
+      value="What Wallets are Currently Supported?"
+      class="mb-2 font-bold"
+    />
+    <TextBody2
+      value="At the moment we’ve integrated Beacon which supports popular Tezos wallets such as Kukai, Temple, and many others."
+      class="mb-4"
+    />
+    <TextBody2
+      value="WI Would Like to Report a Bug or Give Feedback"
+      class="mb-2 font-bold"
+    />
+    <TextBody2
+      value="Please direct any questions or inquiries to hello@spruceid.com."
+      class="mb-4"
+    />
   </div>
 </BasePage>
