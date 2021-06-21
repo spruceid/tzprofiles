967aa518803aca8bdd354424ee4c299ae2c132d1
Simon Bihel <simon.bihel@spruceid.com>
2021-06-21 20:54:55 +0100

    Handle CORS witness worker

--- worker/worker/worker.js
+++ worker/worker/worker.js
@@ -123,9 +123,55 @@ const headers = new Headers({
   'Access-Control-Allow-Origin': '*'
 })
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+}
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  const request = event.request
+  if (request.method === "OPTIONS") {
+    // Handle CORS preflight requests
+    event.respondWith(handleOptions(request))
+  } else {
+    event.respondWith(handleRequest(request))
+  }
 })
+
+function handleOptions(request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  let headers = request.headers;
+  if (
+    headers.get("Origin") !== null &&
+    headers.get("Access-Control-Request-Method") !== null &&
+    headers.get("Access-Control-Request-Headers") !== null
+  ){
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    let respHeaders = {
+      ...corsHeaders,
+    // Allow all future content Request headers to go back to browser
+    // such as Authorization (Bearer) or X-Client-Name-Version
+      "Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers"),
+    }
+
+    return new Response(null, {
+      headers: respHeaders,
+    })
+  }
+  else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: "GET, OPTIONS",
+      },
+    })
+  }
+}
 
 async function handler_witness_tweet(request) {
   try {
