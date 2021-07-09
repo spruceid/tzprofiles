a2487c52399f6e78e14a249ca6798aa3d05df94a
Kevin Z <zkw917@gmail.com>
2021-07-09 00:17:03 -0400

    Add discord auth route and basic setup:

--- worker/README.md
+++ worker/README.md
@@ -6,24 +6,29 @@ A Cloudflare Worker to witness a tweet claiming ownership of a Tezos account.
 
 Requires [Wrangler](https://developers.cloudflare.com/workers/cli-wrangler/install-update#additional-installation-instructions).
 
-You need a Cloudflare account, and the Account ID needs to go in 
+You need a Cloudflare account, and the Account ID needs to go in
 the `wrangler.toml`.
 
 The worker needs two things: the private key to issue the VC after verifying the
 tweet, and an API token for the Twitter API. You can add these as secrets with:
+
 ```bash
 wrangler secret put TZPROFILES_ME_PRIVATE_KEY
 wrangler secret put TWITTER_BEARER_TOKEN
+wrangler secret put DISCORD_AUTHORIZATION_TOKEN
 ```
+
 > The private key is expected to be a JWK. You can generate one with
 > `didkit generate-ed25519-key`.
 
 ```bash
 wrangler publish
 ```
+
 > For development, you should use `wrangler dev`.
 
 ## Test
+
 ```bash
 cargo test
 ```
--- worker/src/discord.rs
+++ worker/src/discord.rs
@@ -1,9 +1,53 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use reqwest::header::{HeaderMap, AUTHORIZATION};
 use serde::Deserialize;
 use url::Url;
 
-// pub async fn retrieve_discord_message(api_token: String, channel_id: String, messageId: String) {
-//     let mut headers = HeaderMap::new();
-//     headers.insert(AUTHORIZATION, format!("Bearer {}", &api_token).parse()?));
-// }
+#[derive(Deserialize)]
+pub struct DiscordResponse {
+    pub id: String,
+    pub content: String,
+    pub channel_id: String,
+    pub author: Author,
+    pub attachments: Vec<String>,
+    pub embeds: Vec<String>,
+    pub mentions: Vec<String>,
+    pub mention_roles: Vec<String>,
+    pub pinned: bool,
+    pub tts: bool,
+    pub timestamp: String,
+    pub edited_timestamp: String,
+    pub flags: u64,
+    pub components: Vec<String>,
+}
+
+#[derive(Deserialize)]
+pub struct Author {
+    pub id: String,
+    pub username: String,
+    pub avatar: String,
+    pub discriminator: String,
+    pub public_flags: u64,
+}
+
+pub async fn retrieve_discord_message(
+    api_token: String,
+    channel_id: String,
+    message_id: String,
+) -> Result<DiscordResponse> {
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, format!("Bot {}", &api_token).parse()?);
+    let client = reqwest::Client::new();
+    let requestUrl = format!(
+        "https://discord.com/api/channels/{}/messages/{}",
+        channel_id, message_id
+    );
+    let res = client
+        .get(Url::parse(&requestUrl)?)
+        .headers(headers)
+        .send()
+        .await?
+        .json()
+        .await?;
+    Ok(res)
+}
