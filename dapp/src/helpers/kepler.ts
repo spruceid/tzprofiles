8478aee37b72245ec6aed5b6a3212a8e17445e94
Kevin Z <zkw917@gmail.com>
2021-07-28 21:20:38 -0400

    New changes

--- dapp/src/helpers/kepler.ts
+++ dapp/src/helpers/kepler.ts
@@ -6,62 +6,30 @@ export const addToKepler = async (
   orbit: string,
   ...obj: Array<any>
 ) => {
-  try {
-    obj.forEach((o) => console.log(o));
-    if (kepler) {
-      // Get around the error of possibly passing nothing.
-      let f = obj.pop();
-      if (!f) {
-        throw new Error('Empty array passed to saveToKepler');
-      }
-
-      const res = await kepler.put(orbit, f, ...obj);
-      if (!res.ok || res.status !== 200) {
-        throw new Error(`Failed to create orbit: ${res.statusText}`);
-      }
-
-      const addresses = await res.text();
-
-      return addresses.split('\n');
-    }
-
-    throw new Error('No Kepler integration found');
-  } catch (error) {
-    console.log('ERROR', error);
-
-    // try to create a orbitID
+  obj.forEach((o) => console.log(o));
+  if (kepler) {
+    // Get around the error of possibly passing nothing.
     let f = obj.pop();
     if (!f) {
       throw new Error('Empty array passed to saveToKepler');
     }
 
-    try {
-      const res = await kepler.createOrbit(f, ...obj);
-      if (!res.ok || res.status !== 200) {
-        throw new Error(`Failed to create orbit: ${res.statusText}`);
-      }
-      const addresses = await res.text();
-
-      return addresses.split('\n');
-    } catch (e) {
-      console.warn(
-        `Failed in create new orbit with error: ${
-          e?.message || JSON.stringify(e)
-        }`
-      );
-      console.warn('Trying existing orbit');
-      try {
-        const ownPkh = await fetchPkh();
-        let id = await getOrbitId(ownPkh, {
-          domain: process.env.KEPLER_URL,
-          index: 0,
-        });
-        return await addToKepler(kepler, id, ...[f, ...obj]);
-      } catch (err) {
-        throw err;
-      }
+    const res = await kepler.put(orbit, f, ...obj);
+    console.log('RES', res);
+    // if (res.status === 404) {
+    //   const ownPkh = await fetchPkh();
+    //   await saveToKepler(kepler, ownPkh, ...[f, ...obj]);
+    // }
+    if (!res.ok || res.status !== 200) {
+      throw new Error(`Failed to create orbit: ${res.statusText}`);
     }
+
+    const addresses = await res.text();
+
+    return addresses.split('\n');
   }
+
+  throw new Error('No Kepler integration found');
 };
 
 export const saveToKepler = async (
