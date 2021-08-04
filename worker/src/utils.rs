767167f94f09c8d3c4c810e6096a838d4e500109
Kevin Z <40219813+kevinz917@users.noreply.github.com>
2021-08-04 15:16:49 -0400

    DNS Worker (#143)
    * Migrate changes over to separate PR
    
    * Change trace to error on logging
    
    * Address PR comments
    
    * Clean up variable naming
    
    * Add check for trimming:
    
    * Add check for start and end with quotation

--- worker/src/utils.rs
+++ worker/src/utils.rs
@@ -2,18 +2,3 @@ extern crate console_error_panic_hook;
 use anyhow::{anyhow, Result};
 
 pub use self::console_error_panic_hook::set_once as set_panic_hook;
-
-pub fn extract_signature(text: String) -> Result<(String, String)> {
-    let mut sig_target = "".to_string();
-    for line in text.split('\n').collect::<Vec<&str>>() {
-        if line.starts_with("sig:") {
-            if sig_target != "" {
-                return Ok((sig_target, line[4..].to_string().clone()));
-            } else {
-                return Err(anyhow!("Signature target is empty."));
-            }
-        }
-        sig_target = format!("{}{}\n", sig_target, line);
-    }
-    Err(anyhow!("Signature not found in tweet."))
-}
