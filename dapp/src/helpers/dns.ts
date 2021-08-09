bb6857eeb449a725b7be74ed295fee34b4d58914
Kevin Z <zkw917@gmail.com>
2021-08-09 12:27:27 -0400

    Add URL validation

--- dapp/src/helpers/dns.ts
+++ dapp/src/helpers/dns.ts
@@ -31,14 +31,6 @@ export const verifyDnsInfo = async (
 };
 
 export const isValidUrl = (str: string): boolean => {
-  var pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-    'i'
-  );
+  var pattern = new RegExp('^(([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}$');
   return !!pattern.test(str);
 };
