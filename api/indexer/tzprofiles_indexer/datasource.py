d0b9be9a4c99995a0335b204aaf1a1e713a0a4ab
Lev Gorodetskiy <github@droserasprout.space>
2022-04-07 15:49:44 +0400

    Bump

---
+++ api/indexer/tzprofiles_indexer/datasource.py
@@ -0,0 +1,16 @@
+from typing import Any, Dict
+
+from dipdup.datasources.datasource import Datasource
+
+
+# TODO: For DipDup: inject datasource classes?
+class TZPDatasource(Datasource):
+    async def run(self) -> None:
+        await self._http.__aenter__()
+
+    async def resolve(self, address: str) -> Dict[str, Any]:
+        return await self._http.request(
+            method='get',
+            url=f'{self._http._url}/{address}',
+            params={"invalid": "true", "valid": "true"},
+        )
